ea8479c24441b76e29687594d5604d8c7bf41f95
Dawid Lapczynski <dawid.lapczynski@actia.se>
2022-04-05 19:00:42 +0200

    [lua] move plugin list

--- init.vim
+++ init.vim
@@ -3,7 +3,6 @@ let g:python3_host_prog = '$HOME/.venv/neovim_p3/bin/python'
 let g:python_host_prog = '$HOME/.venv/neovim_p2/bin/python'
 
 source $HOME/.config/nvim/general.vim
-source $HOME/.config/nvim/plugins/list.vim
 
 lua require('plugins')
 lua require('colorscheme')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,7 +1,23 @@
--- local Plug = vim.fn['plug#']
+local Plug = vim.fn['plug#']
 
--- vim.call('plug#begin', '$HOME/.local/share/nvim/plugged')
+vim.call('plug#begin', '$HOME/.local/share/nvim/plugged')
 
--- Plug 'windwp/nvim-autopairs'
-
--- vim.call('plug#end')
+Plug 'windwp/nvim-autopairs'
+Plug 'tpope/vim-commentary'
+Plug 'matfranczyk/highlighter.vim'
+Plug 'yamatsum/nvim-cursorline'
+--git
+Plug 'tpope/vim-fugitive'
+Plug 'lewis6991/gitsigns.nvim'
+--utilities
+Plug 'bronson/vim-trailing-whitespace'
+--fzf
+Plug('junegunn/fzf', { ['do'] = vim.fn['fzf#install'] })
+Plug 'junegunn/fzf.vim'
+Plug 'zackhsi/fzf-tags'
+--lsp
+Plug 'ojroques/nvim-lspfuzzy'
+Plug 'neovim/nvim-lspconfig'
+--layout
+Plug 'morhetz/gruvbox'
+vim.call('plug#end')
