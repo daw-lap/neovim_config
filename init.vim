a91bed80c60fcf16fc2845f437f961d3fe88d83a
Dawid Lapczynski <dawid.lapczynski@actia.se>
2022-04-05 20:07:18 +0200

    [lua] general settings moved

--- init.vim
+++ init.vim
@@ -2,10 +2,10 @@
 let g:python3_host_prog = '$HOME/.venv/neovim_p3/bin/python'
 let g:python_host_prog = '$HOME/.venv/neovim_p2/bin/python'
 
-source $HOME/.config/nvim/general.vim
-
 lua require('plugins')
 lua require('colorscheme')
+lua require('general')
+lua require('filetype-settings')
 lua require('autopairs-settings')
 lua require('fzf-settings')
 lua require('lsp-settings')
---
+++ lua/filetype-settings.lua
@@ -0,0 +1,13 @@
+vim.cmd [[
+  autocmd BufNewFile,BufRead *.log setfiletype log
+  autocmd BufNewFile,BufRead *.LOG setfiletype log
+  autocmd BufNewFile,BufRead *.out setfiletype log
+  autocmd BufNewFile,BufRead *.journal setfiletype messages
+  autocmd Filetype cpp,c setlocal colorcolumn=120
+  autocmd Filetype python setlocal colorcolumn=90
+]]
+
+local map = vim.api.nvim_set_keymap
+
+map('n', '<space>l', ':set filetype=log<CR>', { noremap = true})
+map('n', '<space>j', ':set filetype=messages<CR>', { noremap = true})
