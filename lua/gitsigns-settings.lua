79b701405330ce91d799af7fc874dac603a78150
daw-lap <dawid.lapczynski@gmail.com>
2023-10-13 21:55:49 +0200

    add mapping for gitsigns

--- lua/gitsigns-settings.lua
+++ lua/gitsigns-settings.lua
@@ -1,6 +1,18 @@
 local config = {
   signcolumn=true,
-  numhl=true
+  numhl=true,
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    map('n', "<space>b", gs.toggle_current_line_blame)
+    map('n', '<space>d', gs.diffthis)
+  end
 }
 
 return config
