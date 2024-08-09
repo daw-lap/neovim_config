c89d9ff05267ec27c53ba0b48c2d56044072c034
dl1987 <dawid.lapczynski@gmail.com>
2024-08-09 17:32:47 +0200

    toggle tab settings for different projects

--- lua/general.lua
+++ lua/general.lua
@@ -13,3 +13,20 @@ vim.opt.incsearch = true
 vim.opt.hlsearch = true
 
 vim.g.mapleader = ','
+
+function ToggleTabSettings()
+  if vim.o.tabstop == 8 then
+    vim.o.tabstop = 2
+    vim.o.shiftwidth = 2
+    vim.o.softtabstop = 2
+    vim.o.expandtab = true
+  else
+    vim.o.tabstop = 8
+    vim.o.shiftwidth = 8
+    vim.o.softtabstop = 8
+    vim.o.expandtab = false
+  end
+  print("Tab settings toggled")
+end
+
+vim.api.nvim_set_keymap('n', '<Leader>tt', ':lua ToggleTabSettings()<CR>', { noremap = true, silent = true })
